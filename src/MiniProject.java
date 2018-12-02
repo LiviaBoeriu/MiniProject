4da77b413c062af95c7184acac9dd3b6b1ee86e9
Livia Boeriu <boeriulivia@gmail.com>
2018-12-02 18:30:25 +0100

    Created the mainView boxes and the css style sheet

--- src/MiniProject.java
+++ src/MiniProject.java
@@ -7,7 +7,9 @@
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -18,13 +20,33 @@ public class MiniProject extends Application{
     
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //create the border pane main frame
         BorderPane mainWindow = new BorderPane();
         
+        //create an HBox for the header
+        HBox header = new HBox();
+        
+        //create the text for the header
+        Label headerText = new Label("Recipe Picker");
+        
+        //add the label to the top header
+        header.getChildren().add(headerText);
+        
+        //create set the mainWindow Border Pane as the main scene for the view
         Scene scene = new Scene(mainWindow, 1000, 800);
         
-        primaryStage.setTitle("Bar Management System");
+        //set the elements in the mainWindow border pane
+        mainWindow.setTop(header);
+        
+        //set the title of the stage
+        primaryStage.setTitle("Recipe picker");
+        
+        //set the scene for the primary stage
         primaryStage.setScene(scene);
+        
+        //make the stage visible
         primaryStage.show();
+   
     }
     /**
      * @param args the command line arguments
